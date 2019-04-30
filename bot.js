4605ac39a148a8d2c405b70990a4e7dd878f61cd
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2019-04-30 19:46:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready',  () => {
 });
 
 const developers = ['507892843881168897'];
-const adminprefix = "!";
+const adminprefix = "#";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
